3fecd7a499c0e6e00dfd519eaa36af40ca3ce9fa
Leonardo La Rocca <leo_la_rocca@yahoo.it>
2024-09-30 10:55:05 +0200

    feat: add relevant parameters to search api

--- project/scratch.py
+++ project/scratch.py
@@ -1,24 +1,16 @@
 # Scratch file to do random tests
 import google.auth
 import google.auth.transport.requests
-from youtube.client import YouTubeClient
+from project.youtube.client import YouTubeClient
 
 credentials, project = google.auth.default(
 	scopes=[
 		"https://www.googleapis.com/auth/cloud-platform",
 		"https://www.googleapis.com/auth/youtube",
-		"https://www.googleapis.com/auth/youtube.force-ssl",
-		"https://www.googleapis.com/auth/youtube.readonly",
 	]
 )
 
-# auth_request = google.auth.transport.requests.Request()
-# credentials.refresh(auth_request)
-
-# print(credentials)
-# print(project)
-
 yt_client = YouTubeClient(credentials=credentials)
-response = yt_client.search(max_results=100)
+response = yt_client.search_videos(max_results=100)
 for item in response:
 	print(item)
--- project/youtube/client.py
+++ project/youtube/client.py
@@ -9,6 +9,8 @@ from project.youtube.models import SearchResultItem
 
 class YouTubeClient:
 	"""Client to access YouTube's APIs."""
+	# TODO: get information about videos via the videos API
+	# TODO: get information about comments via the commentsThread API
 
 	YOUTUBE_API_SERVICE_NAME: str = "youtube"
 	YOUTUBE_API_VERSION: str = "v3"
@@ -24,14 +26,16 @@ class YouTubeClient:
 			credentials=credentials,
 		)
 
-	def search(
+	def search_videos(
 		self,
 		max_results: int,
+		duration: Literal["any", "long", "medium", "short"] = "any",
+		order: Literal["date", "rating", "relevance", "title", "viewCount"] = "relevance",
 		published_after: datetime | None = None,
 		published_before: datetime | None = None,
+		query: str | None = None,
 		region_code: str | None = None,
 		relevance_language: str | None = None,
-		result_type: str = "video",
 		safe_search: Literal["moderate", "none", "strict"] = "none",
 		topic_id: str | None = None,
 	) -> list[SearchResultItem]:
@@ -39,24 +43,29 @@ class YouTubeClient:
 		Full documentation of API available at https://developers.google.com/youtube/v3/docs/search/list.
 
 		:param max_results: retrieve up to this number of results, set to -1 to retrive all the results, use with care.
+		:param duration: filters search results based on their duration. long 20m<, medium 4m<..<20m, short <4m
+		:param order: method that will be used to order resources in the API response.
 		:param published_after: return resources created after this time.
 		:param published_before: return resources created before this time.
+		:param query: specifies the query term to search for. Your request can also use the Boolean NOT (-) and OR (|) operators to exclude videos or to find videos that are associated with one of several search terms. 
 		:param region_code: return search results for videos that can be viewed in the specified country. The parameter value is an ISO 3166-1 alpha-2 country code.
 		:param relevance_language: return search results that are most relevant to the specified language. The parameter value is typically an ISO 639-1 two-letter language code. However, you should use the values zh-Hans for simplified Chinese and zh-Hant for traditional Chinese. Please note that results in other languages will still be returned if they are highly relevant to the search query term.
 		:param safe_search: whether the search results should include restricted content as well as standard content.
 		:param topic_id: response should only contain resources associated with the specified topic. List of available topics at: https://developers.google.com/youtube/v3/docs/search/list
-		:param result_type: only retrieve a particular type of resource. The value is a comma-separated list of resource types. The default value is video.
 		"""
 		request = self.service.search().list(
 			maxResults=50,  # maximum value accepted, why should it be less if we are not worried about bandwidth/data consumption?
+			order=order,
 			part="snippet",
-			publishedAfter=published_after.isoformat(timespec="seconds"),
-			publishedBefore=published_before.isoformat(timespec="seconds"),
+			publishedAfter=published_after.isoformat(timespec="seconds") if published_after else None,
+			publishedBefore=published_before.isoformat(timespec="seconds") if published_before else None,
+			q=query,
 			regionCode=region_code,
 			relevanceLanguage=relevance_language,
 			safeSearch=safe_search,
 			topicId=topic_id,
-			type=result_type,
+			type="video",
+			videoDuration=duration,
 		)
 
 		response = request.execute()
