4ccf1fbd33d52008e0a9167e035aaa01958187b1
Leonardo La Rocca <leo_la_rocca@yahoo.it>
2024-11-12 16:39:22 +0100

    feat: avoid accidentally overwrting younicon-01 dataset

--- scripts/generate_dataset_from_younicon.py
+++ scripts/generate_dataset_from_younicon.py
@@ -1,12 +1,12 @@
 import random
-import pandas as pd
+from datetime import datetime, timezone
 
 import google.auth
 import google.auth.transport.requests
+import pandas as pd
 
 from project.dataset_generation.dataset_generation import generate_dataset_from_video_ids
 from project.youtube.client import YouTubeClient
-
 
 credentials, project = google.auth.default(
 	scopes=[
@@ -19,6 +19,7 @@ yt_client = YouTubeClient(credentials=credentials)
 younicon_video_ids = pd.read_csv("data/YouNiCon/conspiracy_label.csv")["video_id"].to_list()
 
 random.seed(42)
+
 generate_dataset_from_video_ids(
-	yt_client, younicon_video_ids, "data/myyounicon-01", 3, True
+	yt_client, younicon_video_ids, f"data/myyounicon-{datetime.now(timezone.utc)}", 3, True
 )
